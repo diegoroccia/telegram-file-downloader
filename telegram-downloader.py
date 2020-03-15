292d714ca2f3700c5ac1f9e6b7a19d0465060165
Diego Roccia <diego.roccia@gmail.com>
2020-03-16 00:48:33 +0100

    initial commit

--- telegram-downloader.py
+++ telegram-downloader.py
@@ -1,16 +1,14 @@
-from telethon import TelegramClient, sync
-from telethon.tl.functions.contacts import ResolveUsernameRequest
-from telethon.tl.functions.channels import GetFullChannelRequest
-from telethon.tl.functions.messages import GetFullChatRequest
+from telethon import TelegramClient
 from telethon.tl.types import InputMessagesFilterDocument
-from datetime import datetime, date
+from datetime import date
 import glob
-import sqlite3
+
+# import sqlite3
 
 api_id = 998937
 api_hash = "438c3382cff1b6bc01ec948bad04d5f3"
 channel_name = "🇮🇹 eBooksItalia 📚 [ #TeamAlberello🌳 #NoTarocchi ☣️ ]"
-#channel_name = "📚 Gruppo eBooksItalia 📚"
+# channel_name = "📚 Gruppo eBooksItalia 📚"
 # channel_name=':it: eBooksItalia :books: [ #TeamAlberello:deciduous_tree: #NoTarocchi ☣ ]'
 offset = date(year=2019, month=9, day=26)
 
@@ -20,19 +18,17 @@ client.start()
 # ---------------------------------------
 
 
-
-room = [
-    dialog
-    for dialog in client.get_dialogs()
-    if dialog.name == channel_name
-][0]
+room = [dialog for dialog in client.get_dialogs() if dialog.name == channel_name][0]
 
 # for msg in client.iter_messages(channel_name, limit=None, filter=InputMessagesFilterDocument, offset_date=offset):
 for msg in client.iter_messages(room, limit=None, filter=InputMessagesFilterDocument):
     try:
         file_name = msg.media.document.to_dict()["attributes"][0].get("file_name", "")
         if file_name.endswith("epub"):
-            print( f"Downloading {file_name} ( uploaded on {msg.media.document.date} )..." , end="")
+            print(
+                f"Downloading {file_name} ( uploaded on {msg.media.document.date} )...",
+                end="",
+            )
             if glob.glob("books/" + file_name):
                 print("file already present")
                 break
