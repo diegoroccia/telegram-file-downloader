1e4a889789a09390c2615528f1f94a361c85ebdc
Diego Roccia <diego.roccia@gmail.com>
2020-03-16 00:43:28 +0100

    initial commit

---
+++ telegram-downloader.py
@@ -0,0 +1,45 @@
+from telethon import TelegramClient, sync
+from telethon.tl.functions.contacts import ResolveUsernameRequest
+from telethon.tl.functions.channels import GetFullChannelRequest
+from telethon.tl.functions.messages import GetFullChatRequest
+from telethon.tl.types import InputMessagesFilterDocument
+from datetime import datetime, date
+import glob
+import sqlite3
+
+api_id = 998937
+api_hash = "438c3382cff1b6bc01ec948bad04d5f3"
+channel_name = "🇮🇹 eBooksItalia 📚 [ #TeamAlberello🌳 #NoTarocchi ☣️ ]"
+#channel_name = "📚 Gruppo eBooksItalia 📚"
+# channel_name=':it: eBooksItalia :books: [ #TeamAlberello:deciduous_tree: #NoTarocchi ☣ ]'
+offset = date(year=2019, month=9, day=26)
+
+client = TelegramClient("anon", api_id, api_hash)
+
+client.start()
+# ---------------------------------------
+
+
+
+room = [
+    dialog
+    for dialog in client.get_dialogs()
+    if dialog.name == channel_name
+][0]
+
+# for msg in client.iter_messages(channel_name, limit=None, filter=InputMessagesFilterDocument, offset_date=offset):
+for msg in client.iter_messages(room, limit=None, filter=InputMessagesFilterDocument):
+    try:
+        file_name = msg.media.document.to_dict()["attributes"][0].get("file_name", "")
+        if file_name.endswith("epub"):
+            print( f"Downloading {file_name} ( uploaded on {msg.media.document.date} )..." , end="")
+            if glob.glob("books/" + file_name):
+                print("file already present")
+                break
+            else:
+                client.download_media(message=msg, file="books/")
+                print("Done")
+    except KeyError:
+        print("Unable to download ")
+        print(msg.media.document)
+        pass
